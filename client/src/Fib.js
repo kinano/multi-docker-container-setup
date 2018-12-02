33b433c1b54d0b55fa3928c61898e8e41316c7ce
Kinan Faham <k@theSilentCamera.com>
2018-12-01 19:33:55 -0500

    Add the client/server/worker source code. Build the necessary k8s config files

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -35,9 +35,6 @@ class Fib extends Component {
   };
 
   renderSeenIndexes() {
-    if (!this.state.seenIndexes) {
-      return;
-    }
     return this.state.seenIndexes.map(({ number }) => number).join(', ');
   }
 
